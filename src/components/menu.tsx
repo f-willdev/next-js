9a05aa5e8acf24dcbfe97af452a3e5143efab60f
f-willdev <f.willian01@icloud.com>
2025-04-11 12:54:18 -0300

    Link

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export	default function Menu() {
   return (
     <ul>
-      <li>Home</li>
-      <li>Sobre</li>
-      <li>Contato</li>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/sobre">Sobre</Link></li>
+      <li><Link href="/contato">Contato</Link></li>
     </ul>
   );
 }
